7aa699f8613d5894ee6665668836cfc8aba643e6
Amanda <awinks@gmail.com>
2022-09-07 17:21:17 -0500

    Add project files.

---
+++ Inheritance Exercises/Smartphone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technology
+{
+    public class Smartphone : Computer
+    {
+        public int NumberOfSelfies { get; set; }
+        public Smartphone(double ram, double storage, bool hasKeyboard) : base(ram, storage, hasKeyboard)
+        {
+        }
+        public int TakeSelfies(int numberOfSelfies) {
+            return NumberOfSelfies += numberOfSelfies;
+        }
+
+    }
+}
